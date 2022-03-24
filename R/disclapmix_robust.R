64701fde4235b52786b00cb9478efa30a757da40
Mikkel Meyer Andersen <mikl@mikl.dk>
2022-03-24 11:27:22 +0100

    robustness improved

--- R/disclapmix_robust.R
+++ R/disclapmix_robust.R
@@ -1,9 +1,6 @@
 #' Robust fitting
 #' 
 #' A wrapper around `disclapmix()` that tries to avoid errors. 
-#' If `disclapmix()` fails, it tries one iteration with `glm_method` = 'glm.fit' 
-#' and uses the resulting `v_matrix` as `init_v` in the next `disclapmix()` 
-#' with `glm_method = 'internal_coef'`. 
 #' Can sometimes avoid errors with SVD problems happening with 
 #' `glm_method = 'internal_coef'` and `glm_method = 'internal_dev'`.
 #' 
@@ -30,8 +27,62 @@ disclapmix_robust <- function(x, clusters, ...) {
   
   error = function(cond) {
     if (verbose > 0L) {
-      cat(as.character(Sys.time()), ": An error occured. ", 
-        "Now trying glm.fit for first iteration to get sensible init_v.\n", sep = "")
+      cat(as.character(Sys.time()), ": An error occured. Trying different approaches.\n", sep = "")
+    }
+    
+    ########################################################################
+    # First try on only min(5*clusters, round(nrow(x)/2)) clusters
+    # 10 times
+    ########################################################################
+    no_rnd_obs <- min(5L*clusters, round(nrow(x)/2))
+
+    if (verbose > 0L) {
+      cat(as.character(Sys.time()), ": Tries with ", no_rnd_obs, " random observations.\n", sep = "")
+    }
+    
+    for (iter_try in seq_len(10L)) {
+      if (verbose > 0L) {
+        cat(as.character(Sys.time()), ": -------------------------------------------------------\n", sep = "")
+        cat(as.character(Sys.time()), ": Try #", iter_try, " with ", no_rnd_obs, " random observations.\n", sep = "")
+        cat(as.character(Sys.time()), ": -------------------------------------------------------\n", sep = "")
+        cat(as.character(Sys.time()), ": > Start try #", iter_try, "\n", sep = "")
+      }
+      
+      new_x <- x[sample(seq_len(nrow(x)), no_rnd_obs), ]
+      
+      fit_rnd <- tryCatch({
+        disclapmix::disclapmix(new_x, clusters = clusters, ...)
+      },
+      error = function(cond) {
+        return(NULL)
+      })
+      
+      if (verbose > 0L) {
+        cat(as.character(Sys.time()), ": < End try #", iter_try, "\n", sep = "")
+      }
+      
+      if (is(fit_rnd, "disclapmixfit")) {
+        fit_rnd_inity <- tryCatch({
+          disclapmix::disclapmix(x, clusters = clusters, 
+                                 init_y = fit_rnd$y, 
+                                 init_y_method = NULL, ...)
+        },
+        error = function(cond) {
+          return(NULL)
+        })
+
+        if (is(fit_rnd_inity, "disclapmixfit")) {
+          return(fit_rnd_inity)
+        }
+      }
+    }
+    
+    
+    ########################################################################
+    # Else, try glm.fit
+    ########################################################################
+    if (verbose > 0L) {
+      cat(as.character(Sys.time()), ": Now trying glm.fit for first iteration to get sensible init_v.\n", sep = "")
     }
     
     # Ignore errors/warnings, not only 1 iteration
